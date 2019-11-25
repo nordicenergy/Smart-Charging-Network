2462fc7b3f4d640d7ec90d8402a1832df3fe5d29
Adam Staveley <adam.staveley@shareandcharge.com>
2019-11-25 16:15:45 +0100

    program entry point

--- src/config/ocn.ts
+++ src/config/ocn.ts
@@ -19,9 +19,11 @@ export const ocnBridgeConfig: IBridgeConfigurationOptions = {
     ],
     modules: {
         implementation: ModuleImplementation.CUSTOM,
-        receiver: []
+        receiver: [],
+        sender: []
     },
     pluggableAPI: new OcpiBridge(),
     pluggableDB: new OcnDB(),
-    pluggableRegistry: new DefaultRegistry("http://localhost:8544", "0x345ca3e014aaf5dca488057592ee47305d9b3e10")
+    pluggableRegistry: new DefaultRegistry("http://localhost:8544", "0x345ca3e014aaf5dca488057592ee47305d9b3e10"),
+    dryRun: true
 }
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { startBridge } from "ocn-bridge"
+import { ocnBridgeConfig } from "./config/ocn"
+import { startOcpiApi } from "./api/ocpi/ocpi"
+import { backendConfig } from "./config/backend"
+
+const main = async () => {
+    await startBridge(ocnBridgeConfig)
+    await startOcpiApi(backendConfig)
+    console.log(`OCN Bridge reachable at ${ocnBridgeConfig.publicBridgeURL}`)
+    console.log(`OCPI 2.1.1 reachable at ${backendConfig.publicURL}`)
+}
+
+main()
