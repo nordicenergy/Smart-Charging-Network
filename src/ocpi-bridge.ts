87a3d8a8c2f4f739918a07175886fd76e27a91bf
Adam Staveley <adam.staveley@shareandcharge.com>
2019-12-02 13:48:32 +0100

    wire up ocpi bridge

--- src/ocpi-bridge.ts
+++ src/ocpi-bridge.ts
@@ -2,10 +2,14 @@ import { IPluggableAPI, IPluggableDB } from "ocn-bridge";
 import { Locations } from "./api/ocn/locations";
 import { EventEmitter } from "events";
 import { Tariffs } from "./api/ocn/tariffs";
+import { Sessions } from "./api/ocn/sessions";
+import { Cdrs } from "./api/ocn/cdrs";
 
 export class OcpiBridge implements IPluggableAPI {
     public locations: Locations
     public tariffs: Tariffs
+    public sessions: Sessions
+    public cdrs: Cdrs
 
     constructor(
         backendDb: IPluggableDB,
@@ -16,5 +20,7 @@ export class OcpiBridge implements IPluggableAPI {
     ) {
         this.locations = new Locations(backendDb, country_code, party_id, publicIP, events)
         this.tariffs = new Tariffs(backendDb, country_code, party_id, publicIP, events)
+        this.sessions = new Sessions(backendDb, country_code, party_id, publicIP, events)
+        this.cdrs = new Cdrs(backendDb, country_code, party_id, publicIP, events)
     }
 }
