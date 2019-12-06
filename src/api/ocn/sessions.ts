6ec4740543a038bac026462460f87b0f1565e3c5
Adam Staveley <adam.staveley@shareandcharge.com>
2019-12-06 14:33:34 +0100

    session list empty if no body.data

--- src/api/ocn/sessions.ts
+++ src/api/ocn/sessions.ts
@@ -11,6 +11,9 @@ export class Sessions extends Forwarder {
             let endpoint = await this.backendDb.getEndpoint("sessions", "SENDER")
             endpoint += `?date_from=${params.date_from}`
             const result = await this.makeOcpiRequest("GET", endpoint)
+            if (!result) {
+                return []
+            }
             return result.map((session: any) => new Session(session, this.country_code, this.party_id))
         }
 
