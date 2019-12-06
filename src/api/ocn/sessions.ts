0a6436373ddcad11870428cd3530049bc6e8f836
Adam Staveley <adam.staveley@shareandcharge.com>
2019-12-06 13:49:59 +0100

    date_from requirement moved to bridge

--- src/api/ocn/sessions.ts
+++ src/api/ocn/sessions.ts
@@ -1,7 +1,7 @@
 import { Forwarder } from "./forwarder";
 import { ISession } from "ocn-bridge/dist/models/ocpi/session";
 import { Session } from "../../models/translators/session";
-import { IPaginationParams, OcpiError } from "ocn-bridge/dist/models/ocpi/common";
+import { IPaginationParams } from "ocn-bridge/dist/models/ocpi/common";
 
 export class Sessions extends Forwarder {
 
@@ -9,9 +9,6 @@ export class Sessions extends Forwarder {
 
         getList: async (params: IPaginationParams): Promise<ISession[]> => {
             let endpoint = await this.backendDb.getEndpoint("sessions", "SENDER")
-            if (!params.date_from) {
-                throw new OcpiError(2001, "Required date_from missing")
-            }
             endpoint += `?date_from=${params.date_from}`
             const result = await this.makeOcpiRequest("GET", endpoint)
             return result.map((session: any) => new Session(session, this.country_code, this.party_id))
